c93c5482922ac36c29ec6de5613a84eaafb5d56f
Semyon Kirekov <semyon@kirekov.com>
2022-09-03 12:04:09 +0300

    fix

--- src/main/java/com/example/demo/domain/PostWithEmbeddedUUID.java
+++ src/main/java/com/example/demo/domain/PostWithEmbeddedUUID.java
@@ -1,25 +1,17 @@
 package com.example.demo.domain;
 
-import static javax.persistence.CascadeType.PERSIST;
-
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.Value;
 
 @Entity
 @Table(name = "post_embedded_uuid")
@@ -29,14 +21,6 @@ public class PostWithEmbeddedUUID {
   @EmbeddedId
   public PostID id;
   private String title;
-
-  @OneToMany(mappedBy = "post", cascade = PERSIST)
-  private List<Comment> comments = new ArrayList<>();
-
-  public void addComment(Comment comment) {
-    comment.setPost(this);
-    comments.add(comment);
-  }
 
   public static PostWithEmbeddedUUID newPost() {
     final var post = new PostWithEmbeddedUUID();
